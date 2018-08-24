5a4d77261b0d1684e47054dec85328e45a84d11f
Adrian Nungka <adrian@theadx.net>
2018-08-24 22:26:12 +0700

    first commit

---
+++ frmMain.Designer.cs
@@ -0,0 +1,313 @@
+﻿namespace Database_Tools
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMain));
+            this.lblHost = new System.Windows.Forms.Label();
+            this.txtHostname = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtOutputDirectory = new System.Windows.Forms.TextBox();
+            this.btnBackupFilePath = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.cboDatabases = new System.Windows.Forms.ComboBox();
+            this.btnBackup = new System.Windows.Forms.Button();
+            this.btnRefresh = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.btnRestore = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnSelectFile = new System.Windows.Forms.Button();
+            this.txtRestoreFile = new System.Windows.Forms.TextBox();
+            this.folderBrowserDialog = new System.Windows.Forms.FolderBrowserDialog();
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.notifyIcon = new System.Windows.Forms.NotifyIcon(this.components);
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtSolutionPath = new System.Windows.Forms.TextBox();
+            this.btnOpenSolutionPathDialog = new System.Windows.Forms.Button();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.solutionBrowserDialog = new System.Windows.Forms.FolderBrowserDialog();
+            this.treeView = new System.Windows.Forms.TreeView();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lblHost
+            // 
+            this.lblHost.AutoSize = true;
+            this.lblHost.Location = new System.Drawing.Point(13, 13);
+            this.lblHost.Name = "lblHost";
+            this.lblHost.Size = new System.Drawing.Size(29, 13);
+            this.lblHost.TabIndex = 0;
+            this.lblHost.Text = "Host";
+            // 
+            // txtHostname
+            // 
+            this.txtHostname.Location = new System.Drawing.Point(80, 9);
+            this.txtHostname.Name = "txtHostname";
+            this.txtHostname.Size = new System.Drawing.Size(178, 20);
+            this.txtHostname.TabIndex = 1;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 25);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(49, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Directory";
+            // 
+            // txtOutputDirectory
+            // 
+            this.txtOutputDirectory.Location = new System.Drawing.Point(68, 21);
+            this.txtOutputDirectory.Name = "txtOutputDirectory";
+            this.txtOutputDirectory.Size = new System.Drawing.Size(178, 20);
+            this.txtOutputDirectory.TabIndex = 7;
+            // 
+            // btnBackupFilePath
+            // 
+            this.btnBackupFilePath.Location = new System.Drawing.Point(252, 20);
+            this.btnBackupFilePath.Name = "btnBackupFilePath";
+            this.btnBackupFilePath.Size = new System.Drawing.Size(25, 23);
+            this.btnBackupFilePath.TabIndex = 8;
+            this.btnBackupFilePath.Text = "...";
+            this.btnBackupFilePath.UseVisualStyleBackColor = true;
+            this.btnBackupFilePath.Click += new System.EventHandler(this.btnBackupFilePath_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(320, 12);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(53, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Database";
+            // 
+            // cboDatabases
+            // 
+            this.cboDatabases.FormattingEnabled = true;
+            this.cboDatabases.Location = new System.Drawing.Point(387, 8);
+            this.cboDatabases.Name = "cboDatabases";
+            this.cboDatabases.Size = new System.Drawing.Size(178, 21);
+            this.cboDatabases.TabIndex = 10;
+            // 
+            // btnBackup
+            // 
+            this.btnBackup.Location = new System.Drawing.Point(171, 47);
+            this.btnBackup.Name = "btnBackup";
+            this.btnBackup.Size = new System.Drawing.Size(75, 23);
+            this.btnBackup.TabIndex = 11;
+            this.btnBackup.Text = "Backup";
+            this.btnBackup.UseVisualStyleBackColor = true;
+            this.btnBackup.Click += new System.EventHandler(this.btnBackup_Click);
+            // 
+            // btnRefresh
+            // 
+            this.btnRefresh.Image = ((System.Drawing.Image)(resources.GetObject("btnRefresh.Image")));
+            this.btnRefresh.Location = new System.Drawing.Point(264, 8);
+            this.btnRefresh.Name = "btnRefresh";
+            this.btnRefresh.Size = new System.Drawing.Size(25, 23);
+            this.btnRefresh.TabIndex = 12;
+            this.btnRefresh.UseVisualStyleBackColor = true;
+            this.btnRefresh.Click += new System.EventHandler(this.btnRefresh_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnBackup);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.txtOutputDirectory);
+            this.groupBox1.Controls.Add(this.btnBackupFilePath);
+            this.groupBox1.Location = new System.Drawing.Point(12, 37);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(292, 82);
+            this.groupBox1.TabIndex = 13;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Backup";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.btnRestore);
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Controls.Add(this.btnSelectFile);
+            this.groupBox2.Controls.Add(this.txtRestoreFile);
+            this.groupBox2.Location = new System.Drawing.Point(323, 37);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(305, 82);
+            this.groupBox2.TabIndex = 14;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Restore";
+            // 
+            // btnRestore
+            // 
+            this.btnRestore.Location = new System.Drawing.Point(167, 50);
+            this.btnRestore.Name = "btnRestore";
+            this.btnRestore.Size = new System.Drawing.Size(75, 23);
+            this.btnRestore.TabIndex = 15;
+            this.btnRestore.Text = "Restore";
+            this.btnRestore.UseVisualStyleBackColor = true;
+            this.btnRestore.Click += new System.EventHandler(this.btnRestore_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(17, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(23, 13);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "File";
+            // 
+            // btnSelectFile
+            // 
+            this.btnSelectFile.Location = new System.Drawing.Point(248, 21);
+            this.btnSelectFile.Name = "btnSelectFile";
+            this.btnSelectFile.Size = new System.Drawing.Size(25, 23);
+            this.btnSelectFile.TabIndex = 14;
+            this.btnSelectFile.Text = "...";
+            this.btnSelectFile.UseVisualStyleBackColor = true;
+            this.btnSelectFile.Click += new System.EventHandler(this.btnSelectFile_Click);
+            // 
+            // txtRestoreFile
+            // 
+            this.txtRestoreFile.Location = new System.Drawing.Point(64, 23);
+            this.txtRestoreFile.Name = "txtRestoreFile";
+            this.txtRestoreFile.Size = new System.Drawing.Size(178, 20);
+            this.txtRestoreFile.TabIndex = 13;
+            // 
+            // notifyIcon
+            // 
+            this.notifyIcon.BalloonTipIcon = System.Windows.Forms.ToolTipIcon.Info;
+            this.notifyIcon.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon.Icon")));
+            this.notifyIcon.Visible = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 158);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(77, 13);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "Solution Folder";
+            // 
+            // txtSolutionPath
+            // 
+            this.txtSolutionPath.Location = new System.Drawing.Point(101, 155);
+            this.txtSolutionPath.Name = "txtSolutionPath";
+            this.txtSolutionPath.Size = new System.Drawing.Size(415, 20);
+            this.txtSolutionPath.TabIndex = 13;
+            // 
+            // btnOpenSolutionPathDialog
+            // 
+            this.btnOpenSolutionPathDialog.Location = new System.Drawing.Point(522, 153);
+            this.btnOpenSolutionPathDialog.Name = "btnOpenSolutionPathDialog";
+            this.btnOpenSolutionPathDialog.Size = new System.Drawing.Size(25, 23);
+            this.btnOpenSolutionPathDialog.TabIndex = 14;
+            this.btnOpenSolutionPathDialog.Text = "...";
+            this.btnOpenSolutionPathDialog.UseVisualStyleBackColor = true;
+            this.btnOpenSolutionPathDialog.Click += new System.EventHandler(this.btnOpenSolutionPathDialog_Click);
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(553, 153);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(75, 23);
+            this.btnSearch.TabIndex = 15;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // treeView
+            // 
+            this.treeView.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.treeView.Location = new System.Drawing.Point(16, 181);
+            this.treeView.Name = "treeView";
+            this.treeView.Size = new System.Drawing.Size(612, 366);
+            this.treeView.TabIndex = 17;
+            this.treeView.NodeMouseDoubleClick += new System.Windows.Forms.TreeNodeMouseClickEventHandler(this.treeViewNode_MouseDoubleClicked);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(640, 559);
+            this.Controls.Add(this.treeView);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtSolutionPath);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.btnOpenSolutionPathDialog);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btnRefresh);
+            this.Controls.Add(this.cboDatabases);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtHostname);
+            this.Controls.Add(this.lblHost);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "frmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Database Tools";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblHost;
+        private System.Windows.Forms.TextBox txtHostname;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtOutputDirectory;
+        private System.Windows.Forms.Button btnBackupFilePath;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox cboDatabases;
+        private System.Windows.Forms.Button btnBackup;
+        private System.Windows.Forms.Button btnRefresh;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button btnRestore;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnSelectFile;
+        private System.Windows.Forms.TextBox txtRestoreFile;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog;
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
+        private System.Windows.Forms.NotifyIcon notifyIcon;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtSolutionPath;
+        private System.Windows.Forms.Button btnOpenSolutionPathDialog;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.FolderBrowserDialog solutionBrowserDialog;
+        private System.Windows.Forms.TreeView treeView;
+    }
+}
+
---
+++ frmMain.cs
@@ -0,0 +1,228 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+namespace Database_Tools
+{
+    public partial class frmMain : Form
+    {
+        private IList<string> _databases;
+
+        private string _directory = Directory.GetCurrentDirectory() + "\\backups";
+
+        public frmMain()
+        {
+            InitializeComponent();
+
+            _databases = new List<string>();
+
+            txtHostname.Text = ".\\SQLEXPRESS";
+
+            if (!Directory.Exists(_directory))
+                Directory.CreateDirectory(_directory);
+
+            txtOutputDirectory.Text = _directory;
+
+            LoadDatabase();
+        }
+
+        public void LoadDatabase()
+        {
+            var proc = ExecuteProcess("sp_databases");
+            
+            _databases.Clear();
+            
+            int i = 0;
+            string input = null;
+
+            Regex reg = new Regex("[\\d\\w-_]+(\\s[\\d\\w-_]+)?");
+            Regex notNeeded = new Regex("master|model|tempdb|msdb");
+
+            while ((input = proc.StandardOutput.ReadLine()) != null)
+            {
+                if (i > 1 && !notNeeded.IsMatch(input))
+                    _databases.Add(reg.Match(input).Value);
+
+                i++;
+            }
+
+            cboDatabases.DataSource = _databases;
+        }
+
+        private void btnRefresh_Click(object sender, EventArgs e)
+        {
+            LoadDatabase();
+        }
+
+        private void btnBackupFilePath_Click(object sender, EventArgs e)
+        {
+            var dir = txtOutputDirectory.Text;
+
+            if (!Directory.Exists(dir))
+                Directory.CreateDirectory(dir);
+
+                folderBrowserDialog.SelectedPath = dir;
+
+            if (folderBrowserDialog.ShowDialog(this) == DialogResult.OK)
+                txtOutputDirectory.Text = folderBrowserDialog.SelectedPath;
+        }
+
+        private void btnBackup_Click(object sender, EventArgs e)
+        {
+            var filename = $"{cboDatabases.SelectedValue}_{DateTime.Now.ToString("yyyyMMddHHmm")}.bak";
+
+            var proc = ExecuteProcess($"BACKUP DATABASE [{cboDatabases.SelectedValue}] TO DISK='{txtOutputDirectory.Text}\\{filename}'");
+
+            var str = proc.StandardOutput.ReadToEnd();
+
+            var strs = str.Split('\n');
+
+            notifyIcon.BalloonTipText = strs[strs.Length - 2];
+            notifyIcon.BalloonTipTitle = "Backup Complete";
+            notifyIcon.ShowBalloonTip(3000);
+        }
+
+        private Process ExecuteProcess(string command)
+        {
+            Process proc = new Process();
+            proc.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+            proc.StartInfo.CreateNoWindow = true;
+            proc.StartInfo.FileName = "sqlcmd";
+            proc.StartInfo.Arguments = $"-S {txtHostname.Text} -Q \"{command}\"";
+            proc.StartInfo.UseShellExecute = false;
+            proc.StartInfo.RedirectStandardOutput = true;
+            proc.Start();
+
+            return proc;
+        }
+
+        private void btnSelectFile_Click(object sender, EventArgs e)
+        {
+            openFileDialog.InitialDirectory = _directory;
+            openFileDialog.Filter = "Backup Files|*.bak";
+            openFileDialog.Title = "Select Backup File";
+
+            if(openFileDialog.ShowDialog(this) == DialogResult.OK)
+                txtRestoreFile.Text = openFileDialog.FileName;
+        }
+
+        private void btnRestore_Click(object sender, EventArgs e)
+        {
+            if (String.IsNullOrEmpty(txtRestoreFile.Text))
+                MessageBox.Show(this, "Restore file not selected!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            else if(!txtRestoreFile.Text.Contains( cboDatabases.SelectedValue.ToString() ))
+                MessageBox.Show(this, "Wrong restore file selected!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            else
+            {
+                var reg = new Regex(@"([\d\w-_]+(\s[\d\w-_]+)?)_[\d]+\.bak");
+
+                var match = reg.Match(txtRestoreFile.Text);
+                var database = match.Groups[1];
+
+                var proc = ExecuteProcess($"RESTORE DATABASE [{database}] FROM DISK='{txtRestoreFile.Text}'");
+
+                var str = proc.StandardOutput.ReadToEnd();
+                
+                var strs = str.Split('\n');
+
+                notifyIcon.BalloonTipText = strs[strs.Length - 2];
+                notifyIcon.BalloonTipTitle = "Restore Complete";
+                notifyIcon.ShowBalloonTip(3000);
+            }
+        }
+       
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            var storeprocs = new List<string>();
+
+            var proc = ExecuteProcess($"USE [{cboDatabases.SelectedValue}]; SELECT ROUTINE_NAME FROM information_schema.routines where ROUTINE_TYPE = 'PROCEDURE'");
+
+            string responses;
+            int index = 0;
+
+            while ((responses = proc.StandardOutput.ReadLine()) != null)
+            {
+                if (index > 2 && !String.IsNullOrEmpty(responses))
+                    storeprocs.Add(responses.Trim());
+                index++;
+            }
+
+            storeprocs.RemoveAt(storeprocs.Count - 1);
+            proc.Dispose();
+            
+            proc = new Process();
+            proc.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+            proc.StartInfo.CreateNoWindow = true;
+            proc.StartInfo.FileName = "cmd";
+            proc.StartInfo.Arguments = $"/C \"dir /S /B {txtSolutionPath.Text}\\*.vb\"";
+            proc.StartInfo.UseShellExecute = false;
+            proc.StartInfo.RedirectStandardOutput = true;
+            proc.Start();
+
+            treeView.Nodes.Clear();
+
+            var rootNode = new TreeNode(txtSolutionPath.Text);
+
+            while((responses = proc.StandardOutput.ReadLine()) != null)
+            {
+                if (!String.IsNullOrEmpty(responses))
+                {
+                    var filename = responses.Trim();
+                    var sp = FindStoreProceduresInFile(storeprocs, filename);
+
+                    if (sp.Count > 0)
+                    {
+                        var parent = new TreeNode(filename.Substring(txtSolutionPath.Text.Trim().Length + 1));
+
+                        foreach (var item in sp)
+                            parent.Nodes.Add(item);
+
+                        rootNode.Nodes.Add(parent);
+                    }
+                }
+            }
+
+            rootNode.ExpandAll();
+            treeView.Nodes.Add(rootNode);
+
+            notifyIcon.BalloonTipText = $"Search Complete.\n{rootNode.GetNodeCount(false)} files found!";
+            notifyIcon.BalloonTipTitle = "Search";
+            notifyIcon.ShowBalloonTip(3000);
+        }
+
+        private void btnOpenSolutionPathDialog_Click(object sender, EventArgs e)
+        {
+            if (solutionBrowserDialog.ShowDialog() == DialogResult.OK)
+            {
+                txtSolutionPath.Text = solutionBrowserDialog.SelectedPath;
+            }
+        }
+
+        private List<string> FindStoreProceduresInFile(IList<string> sp, string file)
+        {
+            var found = new List<string>();
+
+
+            var input = File.ReadAllText(file);
+
+            foreach (var item in sp)
+                if (input.Contains(item))
+                    found.Add(item);
+
+            return found;
+        }
+
+        private void treeViewNode_MouseDoubleClicked(object sender, TreeNodeMouseClickEventArgs e)
+        {
+            var node = treeView.SelectedNode;
+
+            if (node.LastNode == null)
+                node = node.Parent;
+
+            Process.Start(node.FullPath);
+        }
+    }
+}
